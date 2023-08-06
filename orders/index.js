1d09bc6c244ce6e7b0f54edb8ec200151f3de7ff
wallowitch2 <wallowitch2@gmail.com>
2023-08-06 16:19:41 +0900

    fix morgan

--- orders/index.js
+++ orders/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
+const morgan = require('morgan');
 
 
 const fs = require('fs');
