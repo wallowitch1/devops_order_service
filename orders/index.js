b105c6ea3a43f217560fa5d020b2238540c37e02
wallowitch2 <wallowitch2@gmail.com>
2023-08-06 15:21:57 +0900

    add loging

--- orders/index.js
+++ orders/index.js
@@ -8,6 +8,13 @@ const db = require('./models');
 const orderRouter = require('./routes/orders');
 
 const app = express();
+
+// 로그 파일 스트림 생성
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+// morgan을 사용하여 액세스 로그 설정
+app.use(morgan('combined', { stream: accessLogStream }));
+
 app.use(express.json());
 
 app.use(cookieParser());
