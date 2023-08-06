4de46ec6c669b3b83389791e8bdf0c412c2a219b
wallowitch2 <wallowitch2@gmail.com>
2023-08-06 18:02:28 +0900

    add path

--- orders/index.js
+++ orders/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
 const morgan = require('morgan');
+const path = require('path');
 
 
 const fs = require('fs');
